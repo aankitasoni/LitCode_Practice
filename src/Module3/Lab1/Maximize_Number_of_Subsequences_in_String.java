0eb0f8291e96a54a40dda35a4eac5d8cee320d6f
Ankita Soni <ankitasoni018@gmail.com>
2023-11-27 11:40:01 +0530

    Maximize Number of Subsequences in String

---
+++ src/Module3/Lab1/Maximize_Number_of_Subsequences_in_String.java
@@ -0,0 +1,32 @@
+package Module3.Lab1;
+
+import java.util.*;
+
+public class Maximize_Number_of_Subsequences_in_String {
+    public static long maximumSubsequenceCount(String text, String pattern) {
+
+        long result = 0, first = 0, second = 0;
+
+        for (int i = 0; i < text.length(); ++i) {
+            if (text.charAt(i) == pattern.charAt(1)) {
+                result += first;
+                second++;
+            }
+
+            if (text.charAt(i) == pattern.charAt(0))
+                first++;
+        }
+
+        return result + Math.max(first, second);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        String b = input.nextLine();
+
+        long ret = maximumSubsequenceCount(a, b);
+        System.out.println(ret);
+    }
+}
