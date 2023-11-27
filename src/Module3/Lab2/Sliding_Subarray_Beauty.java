f57d3fb97c938b8770cb69bd34aaa82fdc6da15b
Ankita Soni <ankitasoni018@gmail.com>
2023-11-27 11:42:37 +0530

    Sliding Subarray Beauty

---
+++ src/Module3/Lab2/Sliding_Subarray_Beauty.java
@@ -0,0 +1,32 @@
+package Module3.Lab2;
+
+import java.util.*;
+
+public class Sliding_Subarray_Beauty {
+    public static List<Integer> slidingSubarrayBeauty(int[] arr, int k, int n) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i <= arr.length - k; i++) {
+            int[] subarray = Arrays.copyOfRange(arr, i, i + k);
+            Arrays.sort(subarray);
+            result.add(subarray[n - 1]);
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().strip().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int k = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        List<Integer> result = slidingSubarrayBeauty(arr, k, n);
+        for (int beauty : result) {
+            System.out.print(beauty + " ");
+        }
+    }
+}
